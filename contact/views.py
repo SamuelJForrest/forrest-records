7f929b9297f6f85df246facb1df8669771196f15
Sam Forrest <samforrest@live.co.uk>
2022-09-19 19:28:48 +0000

    AMEND: removed unnecessary a tags from nav

--- contact/views.py
+++ contact/views.py
@@ -7,9 +7,11 @@ def contact_us(request):
     Renders the contact page, and handles the form submission
     """
     page_title = "Contact"
+    page_description = "Get in touch"
 
     context = {
-        'page_title': page_title
+        'page_title': page_title,
+        'page_description': page_description
     }
 
     return render(request, 'contact/contact.html', context)
