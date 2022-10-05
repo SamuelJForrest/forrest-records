1cc78e4251c8215052287a33088590f73a4b2d88
Sam Forrest <samforrest@live.co.uk>
2022-10-05 07:32:22 +0000

    IMP: added profile functionality

---
+++ profiles/static/profiles/js/countryfield.js
@@ -0,0 +1,14 @@
+let countrySelected = $('#id_default_country').val();
+
+if(!countrySelected) {
+    $('#id_default_country').css('color', 'grey');
+};
+
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', 'grey');
+    } else {
+        $(this).css('color', '#18181b');
+    };
+});
