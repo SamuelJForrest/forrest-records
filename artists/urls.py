11560422363be0b4040d7cddd8fd4c9248a4a4cc
Sam Forrest <samforrest@live.co.uk>
2022-10-06 19:28:05 +0000

    AMEND: replaced underscores in urls for slashes

--- artists/urls.py
+++ artists/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_artists, name='artists'),
-    path('add_artist', views.add_artist, name='add_artist'),
-    path('edit_artist/<artist_id>', views.edit_artist, name='edit_artist'),
-    path('artist_warning/<artist_id>', views.artist_warning, name='artist_warning'),
-    path('delete_artist/<artist_id>', views.delete_artist, name='delete_artist'),
+    path('add/artist', views.add_artist, name='add_artist'),
+    path('edit/artist/<artist_id>', views.edit_artist, name='edit_artist'),
+    path('artist/warning/<artist_id>', views.artist_warning, name='artist_warning'),
+    path('delete/artist/<artist_id>', views.delete_artist, name='delete_artist'),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.all_blog_posts, name='blog'),
     path('<blog_id>', views.blog_detail, name='blog_detail'),
-    path('add_blog/', views.add_blog, name='add_blog'),
-    path('edit_blog/<blog_id>', views.edit_blog, name='edit_blog'),
-    path('delete_blog/<blog_id>', views.delete_blog, name='delete_blog'),
-    path('feature_blog/<blog_id>', views.feature_blog, name='feature_blog'),
-    path('blog_warning/<blog_id>', views.blog_warning, name='blog_warning'),
+    path('add/blog/', views.add_blog, name='add_blog'),
+    path('edit/blog/<blog_id>', views.edit_blog, name='edit_blog'),
+    path('delete/blog/<blog_id>', views.delete_blog, name='delete_blog'),
+    path('feature/blog/<blog_id>', views.feature_blog, name='feature_blog'),
+    path('blog/warning/<blog_id>', views.blog_warning, name='blog_warning'),
 ]
