ff8e3e5e11d7b410c848eb0588041817e11a7421
Sam Forrest <samforrest@live.co.uk>
2022-10-07 19:47:40 +0000

    AMEND: added default to secret key

--- forrest_records/settings.py
+++ forrest_records/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPMENT')
@@ -132,18 +132,6 @@ WSGI_APPLICATION = 'forrest_records.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# if 'DATABASE_URL' is os.environ:
-#     DATABASES = {
-#         'default': dj_database_url(os.environ.get('DATABASE_URL'))
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
