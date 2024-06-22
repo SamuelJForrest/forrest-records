c47e1c76eeb6f47b3b2ede69cae38ca3a8b730c2
Sam Forrest <samueljamesforrestweb@gmail.com>
2024-06-22 12:25:14 +0100

    AMEND: Amended WSGI file

--- forrest_records/wsgi.py
+++ forrest_records/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/root/projects/forrest_records/forrest-records')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forrest_records.settings')
 
